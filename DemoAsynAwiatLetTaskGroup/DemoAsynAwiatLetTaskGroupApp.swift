1dc72fe7cccae64a52038ac342b166e926af60c7
Hetal Mehta <hetalmehta@vebuin.com>
2024-02-06 11:06:10 +0530

    Initial Commit

---
+++ DemoAsynAwiatLetTaskGroup/DemoAsynAwiatLetTaskGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAsynAwiatLetTaskGroupApp.swift
+//  DemoAsynAwiatLetTaskGroup
+//
+//  Created by Hetal Mehta on 06/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoAsynAwiatLetTaskGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
